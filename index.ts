dec1231f4b90fc6241fe2603a5e0651c73cf62e8
Randi Julio Fajri <randijulio13@gmail.com>
2024-03-28 13:49:02 +0700

    initial commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import { json } from "body-parser";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(json());
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
